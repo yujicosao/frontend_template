2136e42bfd96c9d9c83ae23d75c03959a588ab4d
小棹雄司 <yujikosao@me.com>
2020-07-30 00:20:43 +0900

    first-commit

---
+++ gulpfile.js
@@ -0,0 +1,109 @@
+const gulp = require("gulp");
+const pug = require("gulp-pug");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cssmin = require("gulp-cssmin");
+const uglify = require("gulp-uglify");
+const browsersync = require("browser-sync");
+const notify = require("gulp-notify");
+const plumber = require("gulp-plumber");
+const del = require("del");
+
+const paths = {
+  src: "src",
+  dest: "dest",
+};
+
+//Pug
+gulp.task("html", function () {
+  return gulp
+    .src([paths.src + "/pages/**/*.pug", "!" + paths.src + "/pages/**/_*.pug"])
+    .pipe(
+      plumber({
+        errorHandler: notify.onError("Error: <%= error.message %>"),
+      })
+    )
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(paths.dest));
+});
+
+//Sass
+gulp.task("css", function () {
+  return gulp
+    .src([paths.src + "/sass/**/*.scss", "!" + paths.src + "/sass/**/_*.scss"])
+    .pipe(
+      plumber({
+        errorHandler: notify.onError("Error: <%= error.message %>"),
+      })
+    )
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: "last 2 versions",
+      })
+    )
+    .pipe(cssmin())
+    .pipe(gulp.dest(paths.dest + "/css"));
+});
+
+//JavaScript
+gulp.task("js", function () {
+  return gulp
+    .src(paths.src + "/javascripts/**/*")
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dest + "/javascripts"));
+});
+
+//Image File
+gulp.task("image", function () {
+  return gulp
+    .src(paths.src + "/images/**/*")
+    .pipe(gulp.dest(paths.dest + "/images"));
+});
+
+//Browser Sync
+gulp.task("browser-sync", function (done) {
+  browsersync({
+    server: {
+      //ローカルサーバー起動
+      baseDir: paths.dest,
+    },
+  });
+  done();
+});
+
+//Watch
+gulp.task("watch", function () {
+  const reload = () => {
+    browsersync.reload(); //リロード
+  };
+  gulp.watch(paths.src + "/sass/**/*").on("change", gulp.series("css", reload));
+  gulp
+    .watch(paths.src + "/pages/**/*")
+    .on("change", gulp.series("html", reload));
+  gulp
+    .watch(paths.src + "/javascripts/**/*")
+    .on("change", gulp.series("js", reload));
+  gulp
+    .watch(paths.src + "/images/**/*")
+    .on("change", gulp.series("image", reload));
+});
+
+//Clean
+gulp.task("clean", function (done) {
+  del.sync(paths.dest + "/**", "！" + paths.dest);
+  done();
+});
+
+//Default
+gulp.task(
+  "default",
+  gulp.series(
+    "clean",
+    gulp.parallel("html", "css", "js", "image", "watch", "browser-sync")
+  )
+);
